445739cc5d3a7c9578fae2f1fa503ac4fd85cab3
Jesse <jesseponce233@gmail.com>
2022-05-19 00:12:41 -0500

    putting in some variables and trying to add random password elements

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment code here
-  
+var characterLength = 8;
+var produce = [];
 
 
 // Get references to the #generate element
@@ -7,6 +8,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
   // window prompt 
   
 
@@ -24,6 +26,11 @@ function generatePassword()
     //password length may be from 8 characters to 128
 
    //add the lowercase uppsecase numbers and special characters 
+
+var numbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ];
+// var symbols = [ " @ " '!', '#', '$', '%', '&', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\','`', '|',  '~' ]
+var lowercaseLetters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var uppercaseLetters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 
     //generate a password based on the criteria
 
