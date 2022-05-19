260ebe8a5edde7daea0fbb03d0f65a634fd125c2
Jesse <jesseponce233@gmail.com>
2022-05-19 00:35:59 -0500

    creating the prompt windows on the website as well as making for loops and conditions

--- script.js
+++ script.js
@@ -10,32 +10,63 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
 
   // window prompt 
+  var askBoxes = getCriteria();
   
-
-  
+  if (askBoxes) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-
+  passwordText.value = password; 
   }
+  }
+
 // generate the password loop 
-function generatePassword() 
+function generatePassword() {
+}
 
   //prompt the user for the password criteria
+function getCriteria() {
+    producePass = [];
+    characterLength = parseInt(prompt("How many characters do you want your password to contain?"));
+  
+
 
     //password length may be from 8 characters to 128
+
+    if (characterLength < 8 || characterLength > 128)  {
+      alert("Charecter length has to be a number between 8 - 128");
+      return false; 
+    }
+
 
    //add the lowercase uppsecase numbers and special characters 
 
 var numbers = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ];
-// var symbols = [ " @ " '!', '#', '$', '%', '&', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\','`', '|',  '~' ]
+// symbols =  " @ " '!', '#', '$', '%', '&', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\','`', '|',  '~' 
 var lowercaseLetters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
 var uppercaseLetters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 
     //generate a password based on the criteria
 
+    if (confirm("Would you like your password to contain lowercase letters?")) {
+      producePass = producePass.concat(lowercaseLetters);
+    }
+    if (confirm("Would you like your password to contain uppercase letters?")) {
+      producePass = producePass.concat(uppercaseLetters);
+    }
+    if (confirm("Would you like your password to numbers?")) {
+      producePass = producePass.concat(numbers);
+    }
+    if (confirm("Would you like your password to contain special characters?")) {
+      producePass = producePass.concat(symbols);
+    }
+
+    return true; 
+  }
+
   // display the password in the text box 
 
 // Add event listener to generate button
+
+
 
 generateBtn.addEventListener("click", writePassword);
