633fda82a844d6767a1922c0b55c9fccc149ab37
Jesse <jesseponce233@gmail.com>
2022-05-19 00:03:18 -0500

    downloading the starter code as well as making sudo code adjustments

---
+++ script.js
@@ -0,0 +1,34 @@
+// Assignment code here
+  
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  // window prompt 
+  
+
+  
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+
+  }
+// generate the password loop 
+function generatePassword() 
+
+  //prompt the user for the password criteria
+
+    //password length may be from 8 characters to 128
+
+   //add the lowercase uppsecase numbers and special characters 
+
+    //generate a password based on the criteria
+
+  // display the password in the text box 
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
